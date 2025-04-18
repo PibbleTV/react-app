e7ad24c4aad534d5765cd8d80b37c830a49f4772
TanzerDx <hristoganchev3@gmail.com>
2025-04-18 17:11:20 +0200

    initial commit

--- src/components/streaming/chatbox.tsx
+++ src/components/streaming/chatbox.tsx
@@ -1,7 +1,7 @@
 const ChatBox: React.FC = () => {
   return (
     <div className="flex flex-col w-full h-full overflow-y-auto bg-chatbox text-white text-[1.3rem] p-4%">
-      {[...Array(5)].map((_, i) => (
+      {[...Array(7)].map((_, i) => (
         <div key={i} className="mb-2% mt-2%">
           <h1>User: this is so fun</h1>
         </div>
