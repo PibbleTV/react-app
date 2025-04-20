e1f3434d159676a057a7d44885264d2d9d1960b3
TanzerDx <hristoganchev3@gmail.com>
2025-04-21 01:04:20 +0200

    finished streaming page

--- src/pages/stream.tsx
+++ src/pages/stream.tsx
@@ -7,7 +7,7 @@ import eye from "../../public/eye.png"
 const Stream: React.FC = () => {
   return (
     <>
-    <div className='p-[2.5%]'>
+    <div className='p-[2.5%] pb-0'>
     <div className="flex h-[73vh] gap-3%">
       <div className="flex-1">
         <Videoplayer />
@@ -18,6 +18,7 @@ const Stream: React.FC = () => {
     </div>
 
     <div className='flex flex-row w-[69.8%] mt-[1.5%]'>
+      
       <div className='h-6% w-6%'>
         <img className='rounded-full' src={temp}></img>
       </div>
@@ -27,22 +28,25 @@ const Stream: React.FC = () => {
           <h1 className='text-[1.5rem]'>A very entertaining live broadcast!</h1>
       </div>
 
-      <div className='flex flex-row ml-auto'>
-        <div className='flex flex-col items-center'>
-          <button className="pl-5% pr-5% text-center pt-2% pb-2% text-[1.2rem] bg-defaultBtn">FOLLOW</button>
-          <div className='flex flex-row'>
-            <img className='w-50% h-40%' src={eye}></img>
-            <h1>1</h1>
+
+        <div className='flex flex-row h-10% max-w-50% gap-1%'>
+
+          <div className='flex flex-col h-20% w-20% items-center'>
+            <button className="pl-10% pr-10% text-center pt-2% pb-2% text-[1.2rem] font-bold bg-defaultBtn">FOLLOW</button>
+            <div className='flex flex-row gap-15% mt-6% justify-center items-center'>
+              <img className='w-[30%] h-40%' src={eye}></img>
+              <h1 className='font-bold'>1</h1>
+            </div>
+          </div>
+          <div className='flex flex-col h-20% w-20% items-center'>
+            <button className="pl-10% pr-10% text-center pt-2% pb-2% text-[1.2rem] font-bold bg-defaultBtn">DONATE</button>
+            <div className='flex flex-row gap-15% mt-6% justify-center items-center'>
+              <img className='w-[30%] h-40%' src={like}></img>
+              <h1 className='font-bold'>0</h1>
+            </div>
           </div>
         </div>
-        <div className='flex flex-col items-center'>
-          <button className="pl-5% pr-5% text-center pt-2% pb-2% text-[1.2rem] bg-defaultBtn">DONATE</button>
-          <div className='flex flex-row'>
-            <img className='w-50% h-40%' src={like}></img>
-            <h1>0</h1>
-          </div>
-        </div>
-      </div>
+   
     </div>
 </div>
     </>
