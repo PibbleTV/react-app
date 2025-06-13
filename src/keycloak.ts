fe1efc99a018dc5a4bcc088fc76673fd3a5dd49b
TanzerDx <hristoganchev3@gmail.com>
2025-06-13 02:05:33 +0200

    test

--- src/keycloak.ts
+++ src/keycloak.ts
@@ -1,6 +1,6 @@
 import Keycloak from "keycloak-js";
 
-const hostname = "https://auth.pibbletv.com"; 
+const hostname = "https://auth.24.144.77.108.nip.io"; 
 const keycloak = new Keycloak({
   url: `${hostname}`, 
   realm: "pibble-tv",
